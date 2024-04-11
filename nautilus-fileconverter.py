c524412e3d0d086458ce7b4b34be79f452cbb500
Ihor M <erwaht@gmail.com>
2024-04-11 10:10:20 +0200

    updated version

--- nautilus-fileconverter.py
+++ nautilus-fileconverter.py
@@ -1,4 +1,4 @@
-converterVersion = "001000006" # Change the number if you want to trigger an update.
+converterVersion = "001000007" # Change the number if you want to trigger an update.
 automaticUpdates = True # Replace the "True" with "False" if you don't want automatic updates.
 
 from gi.repository import Nautilus, GObject
